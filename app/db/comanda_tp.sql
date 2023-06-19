61dc8738d21924e43eef6465a4572b115baca7bc
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2023-06-19 17:21:31 -0300

    db

--- app/db/comanda_tp.sql
+++ app/db/comanda_tp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-06-2023 a las 10:37:57
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.0.28
+-- Tiempo de generación: 19-06-2023 a las 22:21:05
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,7 +35,7 @@ CREATE TABLE `facturas` (
   `puntaje` int(2) NOT NULL,
   `encuesta` varchar(66) NOT NULL,
   `fechaBaja` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `facturas`
@@ -57,14 +57,14 @@ CREATE TABLE `mesas` (
   `id` int(11) NOT NULL,
   `codigoMesa` varchar(5) NOT NULL,
   `estado` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `mesas`
 --
 
 INSERT INTO `mesas` (`id`, `codigoMesa`, `estado`) VALUES
-(6, 'sMcNd', 'Esperando Pedido'),
+(6, 'sMcNd', 'Cerrada'),
 (7, 'P9RiB', 'Cerrada'),
 (8, 'K2J5M', 'Cerrada');
 
@@ -85,15 +85,16 @@ CREATE TABLE `pedidos` (
   `tiempoInicio` time DEFAULT NULL,
   `tiempoEntregado` time DEFAULT NULL,
   `fechaBaja` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `pedidos`
 --
 
 INSERT INTO `pedidos` (`id`, `codigoPedido`, `idMesa`, `idProducto`, `nombreCliente`, `estado`, `tiempoEstimado`, `tiempoInicio`, `tiempoEntregado`, `fechaBaja`) VALUES
-(21, 'ycZXJ', 6, 5, 'Pepe', 'Pendiente', NULL, NULL, NULL, NULL),
-(22, 'ycZXJ', 6, 2, 'Pepe', 'Pendiente', NULL, NULL, NULL, NULL);
+(21, 'ycZXJ', 6, 5, 'Pepe', 'Entregado', '00:15:00', '16:58:38', '17:17:55', NULL),
+(22, 'ycZXJ', 6, 2, 'Pepe', 'En preparacion', '00:15:00', '16:59:48', NULL, NULL),
+(23, 'ycZXJ', 6, 2, 'Juan Carlos', 'Pendiente', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -105,20 +106,21 @@ CREATE TABLE `productos` (
   `id` int(11) NOT NULL,
   `descripcion` varchar(50) NOT NULL,
   `tipo` varchar(20) NOT NULL,
-  `precio` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `precio` float NOT NULL,
+  `fechaBaja` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `descripcion`, `tipo`, `precio`) VALUES
-(1, 'Pinta IPA', 'cervecero', 500),
-(2, 'Papas con cheddar', 'cocinero', 800),
-(3, 'Coca-cola', 'bartender', 300),
-(4, 'Sandwich', 'cocinero', 500),
-(5, 'Milanesa Napolitana', 'cocinero', 900),
-(6, 'Hamburguesa Completa', 'cocinero', 1000);
+INSERT INTO `productos` (`id`, `descripcion`, `tipo`, `precio`, `fechaBaja`) VALUES
+(1, 'Pinta IPA', 'cervecero', 500, NULL),
+(2, 'Papas con cheddar', 'cocinero', 800, NULL),
+(3, 'Coca-cola', 'bartender', 300, NULL),
+(4, 'Sandwich de Lomito XL', 'cocinero', 1500, '2023-06-19'),
+(5, 'Milanesa Napolitana', 'cocinero', 900, NULL),
+(6, 'Hamburguesa Completa', 'cocinero', 1000, NULL);
 
 -- --------------------------------------------------------
 
@@ -129,24 +131,25 @@ INSERT INTO `productos` (`id`, `descripcion`, `tipo`, `precio`) VALUES
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `usuario` varchar(50) NOT NULL,
-  `clave` varchar(250) NOT NULL,
+  `clave` varchar(500) NOT NULL,
   `rol` varchar(20) NOT NULL,
   `token` varchar(500) DEFAULT NULL,
   `expiracionToken` time DEFAULT NULL,
   `fechaBaja` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `usuario`, `clave`, `rol`, `token`, `expiracionToken`, `fechaBaja`) VALUES
-(10, 'admin', '$2y$10$GIxPuyjENd6pHSPIVH0d8O8/ri1yhchIHubT1fRsr2oaf7VZp9iPG', 'socio', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcxNTM0NTcsImV4cCI6MTY4NzIxMzQ1NywiYXVkIjoiMjdmNzFjMGU1ODU0M2ZhNWUyYTUxNTJkMDgxOTc0NTJhOGQ4MTJmYyIsImRhdGEiOnsidXN1YXJpbyI6ImFkbWluIiwicm9sIjoic29jaW8iLCJjbGF2ZSI6IiQyeSQxMCRHSXhQdXlqRU5kNnBIU1BJVkgwZDhPOFwvcmkxeWhjaElIdWJUMWZSc3Iyb2FmN1ZacDlpUEcifSwiYXBwIjoiVFAgQ29tYW5kYSJ9.b7CEzMD1ImgjXGRlesSp4597d-Mj-fq0F-Xuth9KpBs', '838:59:59', NULL),
 (11, 'roberto', '$2y$10$raWIEZ1Ix6spjafoeiDrmOhxPa2cIysVFgSiqqUgwb.F/NKMMdhJ6', 'mozo', NULL, NULL, NULL),
-(12, 'pepe', '$2y$10$3j3QWK6pfgC7iFI4oggjieqMSI6Ws0l/Tx1xGTZ3q/0Bq9msT7qeK', 'cocinero', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcxNjMzODYsImV4cCI6MTY4NzIyMzM4NiwiYXVkIjoiMjdmNzFjMGU1ODU0M2ZhNWUyYTUxNTJkMDgxOTc0NTJhOGQ4MTJmYyIsImRhdGEiOnsidXN1YXJpbyI6InBlcGUiLCJyb2wiOiJjb2NpbmVybyIsImNsYXZlIjoiJDJ5JDEwJDNqM1FXSzZwZmdDN2lGSTRvZ2dqaWVxTVNJNldzMGxcL1R4MXhHVFozcVwvMEJxOW1zVDdxZUsifSwiYXBwIjoiVFAgQ29tYW5kYSJ9.lxjtDGcS7R8nkShTgKtk51NX0IdCQJQkY84BkPwTIXo', '00:00:00', NULL),
-(13, 'raul', '$2y$10$1oxYAreORhL6klymQEi.Au565o5hSDRvbCYAkgODVWGd5RwV8mU/G', 'bartender', NULL, NULL, NULL),
-(14, 'tito', '$2y$10$f47Q6fIN1CE3jAiPXboVL.ptSLW4iQlfBKmB9imy1b3A0UdUNiGYO', 'socio', NULL, NULL, NULL),
-(15, 'maria', '$2y$10$NOmxeoGLztJuaeuZLsIyBOjiFEhd404n.g.Eh2F0sJqqn9d.veZMy', 'mozo', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcxNjAwNjAsImV4cCI6MTY4NzIyMDA2MCwiYXVkIjoiMjdmNzFjMGU1ODU0M2ZhNWUyYTUxNTJkMDgxOTc0NTJhOGQ4MTJmYyIsImRhdGEiOnsidXN1YXJpbyI6Im1hcmlhIiwicm9sIjoibW96byIsImNsYXZlIjoiJDJ5JDEwJE5PbXhlb0dMenRKdWFldVpMc0l5Qk9qaUZFaGQ0MDRuLmcuRWgyRjBzSnFxbjlkLnZlWk15In0sImFwcCI6IlRQIENvbWFuZGEifQ.yyj8cWuYbrfPLv10hZF2d2E3ElpmctnkxGH4OcrPEcc', '00:00:00', NULL);
+(12, 'pepe', '$2y$10$qVwOpLpAaTqGhr2OyWEdZuYOswYMmz36ClcQvKCIiDbyO4eXbkA2u', 'cocinero', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcyMDU3MzksImV4cCI6MTY4NzI2NTczOSwiYXVkIjoiMzc5YWM2YzJkZWJjYmY4ZDI3MzI0MWRlZDg4ZjY1M2ZkMWExZDE5NCIsImRhdGEiOnsidXN1YXJpbyI6InBlcGUiLCJyb2wiOiJjb2NpbmVybyIsImNsYXZlIjoiJDJ5JDEwJHFWd09wTHBBYVRxR2hyMk95V0VkWnVZT3N3WU1tejM2Q2xjUXZLQ0lpRGJ5TzRlWGJrQTJ1In0sImFwcCI6IlRQIENvbWFuZGEifQ.YLWkJIamrXH1BWXLumB8ZXfCeTBgIrVa2uZzwDoLYgg', '838:59:59', NULL),
+(13, 'raul', '$2y$10$1oxYAreORhL6klymQEi.Au565o5hSDRvbCYAkgODVWGd5RwV8mU/G', 'bartender', NULL, NULL, '2023-06-19'),
+(14, 'tito', '$2y$10$olXCrpQ9ewHdMUMmibxC9uwQ6D6gmkw2asKN53ERTDpo9jCtRY1Ga', 'cervecero', NULL, NULL, '2023-06-19'),
+(15, 'maria', '$2y$10$4XbEElJHziKYFEse1N4OAOwBPPkRCrif.dx5gteMOVJebPRNxSh3m', 'mozo', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcyMDU3NTAsImV4cCI6MTY4NzI2NTc1MCwiYXVkIjoiMzc5YWM2YzJkZWJjYmY4ZDI3MzI0MWRlZDg4ZjY1M2ZkMWExZDE5NCIsImRhdGEiOnsidXN1YXJpbyI6Im1hcmlhIiwicm9sIjoibW96byIsImNsYXZlIjoiJDJ5JDEwJDRYYkVFbEpIemlLWUZFc2UxTjRPQU93QlBQa1JDcmlmLmR4NWd0ZU1PVkplYlBSTnhTaDNtIn0sImFwcCI6IlRQIENvbWFuZGEifQ.NJ0sJJ8Oh2cKGz6OaxWezrKmO695tPtNA5-uBJSQok0', '838:59:59', NULL),
+(16, 'ana', '$2y$10$XAULPFBjZDuuToITqH82nuF7.MUTaMkd3y91Dc5Asn3XhcTLxa76.', 'candybar', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcyMDQ4MjAsImV4cCI6MTY4NzI2NDgyMCwiYXVkIjoiMzc5YWM2YzJkZWJjYmY4ZDI3MzI0MWRlZDg4ZjY1M2ZkMWExZDE5NCIsImRhdGEiOnsidXN1YXJpbyI6ImFuYSIsInJvbCI6ImNhbmR5YmFyIiwiY2xhdmUiOiIkMnkkMTAkWEFVTFBGQmpaRHV1VG9JVHFIODJudUY3Lk1VVGFNa2QzeTkxRGM1QXNuM1hoY1RMeGE3Ni4ifSwiYXBwIjoiVFAgQ29tYW5kYSJ9.8HAf_q__Ea1nIhfIPBs_XdfT4K2fYxcqzw9x7siTjNk', '838:59:59', NULL),
+(17, 'admin', '$2y$10$fawxBPHy3MzuXrort1Ro7eaARbpno6aVzk66ERssTMvMSd5erGyp2', 'socio', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcyMDM5MjQsImV4cCI6MTY4NzI2MzkyNCwiYXVkIjoiMzc5YWM2YzJkZWJjYmY4ZDI3MzI0MWRlZDg4ZjY1M2ZkMWExZDE5NCIsImRhdGEiOnsidXN1YXJpbyI6ImFkbWluIiwicm9sIjoic29jaW8iLCJjbGF2ZSI6IiQyeSQxMCRmYXd4QlBIeTNNenVYcm9ydDFSbzdlYUFSYnBubzZhVnprNjZFUnNzVE12TVNkNWVyR3lwMiJ9LCJhcHAiOiJUUCBDb21hbmRhIn0.vBng3RRkkcITw-T1Yl76NyhcGGecBPnTF65LwILan4c', '838:59:59', NULL);
 
 --
 -- Índices para tablas volcadas
@@ -202,7 +205,7 @@ ALTER TABLE `mesas`
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -214,7 +217,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
