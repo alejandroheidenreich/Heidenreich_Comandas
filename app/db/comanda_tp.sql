f8cd9b788d14065c8f269ba5d75fa4ae61fa4f77
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2023-06-26 17:55:11 -0300

    db

--- app/db/comanda_tp.sql
+++ app/db/comanda_tp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-06-2023 a las 22:21:05
+-- Tiempo de generación: 26-06-2023 a las 22:53:46
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -24,28 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `facturas`
+-- Estructura de tabla para la tabla `encuestas`
 --
 
-CREATE TABLE `facturas` (
-  `id` int(11) NOT NULL,
-  `precioTotal` int(11) DEFAULT NULL,
-  `estado` varchar(20) NOT NULL,
-  `fotoMesa` varchar(50) NOT NULL,
-  `puntaje` int(2) NOT NULL,
-  `encuesta` varchar(66) NOT NULL,
-  `fechaBaja` date DEFAULT NULL
+CREATE TABLE `encuestas` (
+  `id` int(8) NOT NULL,
+  `codigoMesa` varchar(15) NOT NULL,
+  `codigoPedido` varchar(15) NOT NULL,
+  `puntuacionMesa` int(5) NOT NULL,
+  `puntuacionRestaurante` int(5) NOT NULL,
+  `puntuacionMozo` int(5) NOT NULL,
+  `puntuacionCocinero` int(5) NOT NULL,
+  `experiencia` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `facturas`
+-- Volcado de datos para la tabla `encuestas`
 --
 
-INSERT INTO `facturas` (`id`, `precioTotal`, `estado`, `fotoMesa`, `puntaje`, `encuesta`, `fechaBaja`) VALUES
-(1, NULL, 'Pendiente', '', 0, '', NULL),
-(2, NULL, 'Pendiente', '', 0, '', NULL),
-(3, NULL, 'Pendiente', '', 0, '', NULL),
-(4, NULL, 'Pendiente', '', 0, '', NULL);
+INSERT INTO `encuestas` (`id`, `codigoMesa`, `codigoPedido`, `puntuacionMesa`, `puntuacionRestaurante`, `puntuacionMozo`, `puntuacionCocinero`, `experiencia`) VALUES
+(1, 'P9RiB', 'HNoxo', 7, 8, 9, 8, 'La pase muy bien, rica comida'),
+(2, 'K2J5M', 'IdzMF', 2, 4, 5, 1, 'No me gusto');
 
 -- --------------------------------------------------------
 
@@ -65,8 +64,9 @@ CREATE TABLE `mesas` (
 
 INSERT INTO `mesas` (`id`, `codigoMesa`, `estado`) VALUES
 (6, 'sMcNd', 'Cerrada'),
-(7, 'P9RiB', 'Cerrada'),
-(8, 'K2J5M', 'Cerrada');
+(7, 'P9RiB', 'Pagando'),
+(8, 'K2J5M', 'Pagando'),
+(9, 'FxnCu', 'Cerrada');
 
 -- --------------------------------------------------------
 
@@ -77,6 +77,7 @@ INSERT INTO `mesas` (`id`, `codigoMesa`, `estado`) VALUES
 CREATE TABLE `pedidos` (
   `id` int(11) NOT NULL,
   `codigoPedido` varchar(5) NOT NULL,
+  `fotoMesa` varchar(150) DEFAULT NULL,
   `idMesa` int(8) NOT NULL,
   `idProducto` int(11) NOT NULL,
   `nombreCliente` varchar(50) NOT NULL,
@@ -91,10 +92,13 @@ CREATE TABLE `pedidos` (
 -- Volcado de datos para la tabla `pedidos`
 --
 
-INSERT INTO `pedidos` (`id`, `codigoPedido`, `idMesa`, `idProducto`, `nombreCliente`, `estado`, `tiempoEstimado`, `tiempoInicio`, `tiempoEntregado`, `fechaBaja`) VALUES
-(21, 'ycZXJ', 6, 5, 'Pepe', 'Entregado', '00:15:00', '16:58:38', '17:17:55', NULL),
-(22, 'ycZXJ', 6, 2, 'Pepe', 'En preparacion', '00:15:00', '16:59:48', NULL, NULL),
-(23, 'ycZXJ', 6, 2, 'Juan Carlos', 'Pendiente', NULL, NULL, NULL, NULL);
+INSERT INTO `pedidos` (`id`, `codigoPedido`, `fotoMesa`, `idMesa`, `idProducto`, `nombreCliente`, `estado`, `tiempoEstimado`, `tiempoInicio`, `tiempoEntregado`, `fechaBaja`) VALUES
+(34, 'IdzMF', './img/2023-06-26_10-16-02_Roberto_Mesa_8.jpg', 8, 57, 'Roberto', 'Entregado', '00:25:00', '10:17:23', '10:19:49', NULL),
+(35, 'IdzMF', './img/2023-06-26_10-16-09_Roberto_Mesa_8.jpg', 8, 60, 'Roberto', 'Pendiente', NULL, NULL, NULL, NULL),
+(36, 'IdzMF', './img/2023-06-26_10-16-19_Maria_Mesa_8.jpg', 8, 59, 'Maria', 'Pendiente', NULL, NULL, NULL, NULL),
+(37, 'IdzMF', './img/2023-06-26_10-16-25_Maria_Mesa_8.jpg', 8, 58, 'Maria', 'En preparacion', '00:25:00', '10:17:28', NULL, NULL),
+(38, 'IdzMF', './img/2023-06-26_10-16-35_Lucia_Mesa_8.jpg', 8, 58, 'Lucia', 'En preparacion', '00:25:00', '10:17:31', NULL, NULL),
+(39, 'HNoxo', './img/2023-06-26_12-08-53_Sofia_Mesa_7.jpg', 7, 58, 'Sofia', 'Entregado', '00:25:00', '12:11:21', '12:11:46', NULL);
 
 -- --------------------------------------------------------
 
@@ -120,7 +124,11 @@ INSERT INTO `productos` (`id`, `descripcion`, `tipo`, `precio`, `fechaBaja`) VAL
 (3, 'Coca-cola', 'bartender', 300, NULL),
 (4, 'Sandwich de Lomito XL', 'cocinero', 1500, '2023-06-19'),
 (5, 'Milanesa Napolitana', 'cocinero', 900, NULL),
-(6, 'Hamburguesa Completa', 'cocinero', 1000, NULL);
+(6, 'Hamburguesa Completa', 'cocinero', 1000, NULL),
+(57, 'Milanesa a Caballo', 'cocinero', 1500, NULL),
+(58, 'Hamburguesa de Garbanzo', 'cocinero', 2500, NULL),
+(59, 'Daikiri', 'bartender', 3000, NULL),
+(60, 'Corona', 'cervecero', 1000, NULL);
 
 -- --------------------------------------------------------
 
@@ -133,8 +141,6 @@ CREATE TABLE `usuarios` (
   `usuario` varchar(50) NOT NULL,
   `clave` varchar(500) NOT NULL,
   `rol` varchar(20) NOT NULL,
-  `token` varchar(500) DEFAULT NULL,
-  `expiracionToken` time DEFAULT NULL,
   `fechaBaja` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -142,23 +148,28 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `clave`, `rol`, `token`, `expiracionToken`, `fechaBaja`) VALUES
-(11, 'roberto', '$2y$10$raWIEZ1Ix6spjafoeiDrmOhxPa2cIysVFgSiqqUgwb.F/NKMMdhJ6', 'mozo', NULL, NULL, NULL),
-(12, 'pepe', '$2y$10$qVwOpLpAaTqGhr2OyWEdZuYOswYMmz36ClcQvKCIiDbyO4eXbkA2u', 'cocinero', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcyMDU3MzksImV4cCI6MTY4NzI2NTczOSwiYXVkIjoiMzc5YWM2YzJkZWJjYmY4ZDI3MzI0MWRlZDg4ZjY1M2ZkMWExZDE5NCIsImRhdGEiOnsidXN1YXJpbyI6InBlcGUiLCJyb2wiOiJjb2NpbmVybyIsImNsYXZlIjoiJDJ5JDEwJHFWd09wTHBBYVRxR2hyMk95V0VkWnVZT3N3WU1tejM2Q2xjUXZLQ0lpRGJ5TzRlWGJrQTJ1In0sImFwcCI6IlRQIENvbWFuZGEifQ.YLWkJIamrXH1BWXLumB8ZXfCeTBgIrVa2uZzwDoLYgg', '838:59:59', NULL),
-(13, 'raul', '$2y$10$1oxYAreORhL6klymQEi.Au565o5hSDRvbCYAkgODVWGd5RwV8mU/G', 'bartender', NULL, NULL, '2023-06-19'),
-(14, 'tito', '$2y$10$olXCrpQ9ewHdMUMmibxC9uwQ6D6gmkw2asKN53ERTDpo9jCtRY1Ga', 'cervecero', NULL, NULL, '2023-06-19'),
-(15, 'maria', '$2y$10$4XbEElJHziKYFEse1N4OAOwBPPkRCrif.dx5gteMOVJebPRNxSh3m', 'mozo', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcyMDU3NTAsImV4cCI6MTY4NzI2NTc1MCwiYXVkIjoiMzc5YWM2YzJkZWJjYmY4ZDI3MzI0MWRlZDg4ZjY1M2ZkMWExZDE5NCIsImRhdGEiOnsidXN1YXJpbyI6Im1hcmlhIiwicm9sIjoibW96byIsImNsYXZlIjoiJDJ5JDEwJDRYYkVFbEpIemlLWUZFc2UxTjRPQU93QlBQa1JDcmlmLmR4NWd0ZU1PVkplYlBSTnhTaDNtIn0sImFwcCI6IlRQIENvbWFuZGEifQ.NJ0sJJ8Oh2cKGz6OaxWezrKmO695tPtNA5-uBJSQok0', '838:59:59', NULL),
-(16, 'ana', '$2y$10$XAULPFBjZDuuToITqH82nuF7.MUTaMkd3y91Dc5Asn3XhcTLxa76.', 'candybar', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcyMDQ4MjAsImV4cCI6MTY4NzI2NDgyMCwiYXVkIjoiMzc5YWM2YzJkZWJjYmY4ZDI3MzI0MWRlZDg4ZjY1M2ZkMWExZDE5NCIsImRhdGEiOnsidXN1YXJpbyI6ImFuYSIsInJvbCI6ImNhbmR5YmFyIiwiY2xhdmUiOiIkMnkkMTAkWEFVTFBGQmpaRHV1VG9JVHFIODJudUY3Lk1VVGFNa2QzeTkxRGM1QXNuM1hoY1RMeGE3Ni4ifSwiYXBwIjoiVFAgQ29tYW5kYSJ9.8HAf_q__Ea1nIhfIPBs_XdfT4K2fYxcqzw9x7siTjNk', '838:59:59', NULL),
-(17, 'admin', '$2y$10$fawxBPHy3MzuXrort1Ro7eaARbpno6aVzk66ERssTMvMSd5erGyp2', 'socio', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODcyMDM5MjQsImV4cCI6MTY4NzI2MzkyNCwiYXVkIjoiMzc5YWM2YzJkZWJjYmY4ZDI3MzI0MWRlZDg4ZjY1M2ZkMWExZDE5NCIsImRhdGEiOnsidXN1YXJpbyI6ImFkbWluIiwicm9sIjoic29jaW8iLCJjbGF2ZSI6IiQyeSQxMCRmYXd4QlBIeTNNenVYcm9ydDFSbzdlYUFSYnBubzZhVnprNjZFUnNzVE12TVNkNWVyR3lwMiJ9LCJhcHAiOiJUUCBDb21hbmRhIn0.vBng3RRkkcITw-T1Yl76NyhcGGecBPnTF65LwILan4c', '838:59:59', NULL);
+INSERT INTO `usuarios` (`id`, `usuario`, `clave`, `rol`, `fechaBaja`) VALUES
+(11, 'roberto', '$2y$10$raWIEZ1Ix6spjafoeiDrmOhxPa2cIysVFgSiqqUgwb.F/NKMMdhJ6', 'mozo', NULL),
+(12, 'pepe', '$2y$10$qVwOpLpAaTqGhr2OyWEdZuYOswYMmz36ClcQvKCIiDbyO4eXbkA2u', 'cocinero', NULL),
+(13, 'carlos', '$2y$10$H/yyqXxsEdiFOurxV2mQr.SGyFq8tj/uYYKwVSjmgGAJEs4Rp.GK6', 'bartender', NULL),
+(14, 'tito', '$2y$10$olXCrpQ9ewHdMUMmibxC9uwQ6D6gmkw2asKN53ERTDpo9jCtRY1Ga', 'cervecero', '2023-06-19'),
+(15, 'maria', '$2y$10$4XbEElJHziKYFEse1N4OAOwBPPkRCrif.dx5gteMOVJebPRNxSh3m', 'mozo', NULL),
+(16, 'ana', '$2y$10$XAULPFBjZDuuToITqH82nuF7.MUTaMkd3y91Dc5Asn3XhcTLxa76.', 'candybar', NULL),
+(17, 'admin', '$2y$10$fawxBPHy3MzuXrort1Ro7eaARbpno6aVzk66ERssTMvMSd5erGyp2', 'socio', NULL),
+(19, 'mozo', '$2y$10$VBPiqxOGMedxrHYljxQHlOcA7UF2vmwGD.7IzIPRbEOgYsjpetQru', 'mozo', NULL),
+(20, 'cocinero', '$2y$10$OFmwx3XcnN/7FLzcCklhAO0a6JH/KzICxfTxTmsD0yMyV4GKYNk0q', 'cocinero', NULL),
+(21, 'bartender', '$2y$10$Hhub3IFBQahsfaMyA9OShO4Dc44qy0ej1OjZuEIGKoegzs0Z0klRm', 'bartender', NULL),
+(22, 'socio', '$2y$10$xv89ELdsCxz76CjHVzWtOe08reVyAqSdVs46VyoBa6GEZZozWSjRy', 'socio', NULL),
+(23, 'cervecero', '$2y$10$E25tZ.G06N9mEINQD7aB2.euhRttdgoH2g.N5SoRi2BdU71V0dRyq', 'cervecero', NULL);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `facturas`
+-- Indices de la tabla `encuestas`
 --
-ALTER TABLE `facturas`
+ALTER TABLE `encuestas`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -190,34 +201,34 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `facturas`
+-- AUTO_INCREMENT de la tabla `encuestas`
 --
-ALTER TABLE `facturas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `encuestas`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `mesas`
 --
 ALTER TABLE `mesas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
